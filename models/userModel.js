ee18636286cb5bc86a10ef5812c819d4fd582c98
prezman nina <prezmannina1989@gmail.com>
2022-11-27 18:16:05 +0200

    bbb

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 
 let userSchema = new mongoose.Schema({
@@ -20,9 +21,34 @@ let userSchema = new mongoose.Schema({
     abonnement: String,
 
 
-    location: String,
-    abonnement: String,
+    location: String
+
 
 })
 
 exports.UserModel = mongoose.model("users", userSchema);
+
+exports.validUser = (_reqBody) => {
+    let joiSchema = Joi.object({
+        name: Joi.string().min(2).max(99).required(),
+        email: Joi.string().min(2).max(99).email().required(),
+        password: Joi.string().min(3).max(99).required(),
+        phone: Joi.string().min(8).max(99).required(),
+        birth_date: Joi.string().min(2).max(99).required(),
+        abonnement: Joi.string().min(2).max(99).required(),
+        img_url: Joi.string().min(2).max(99).allow(null, ""),
+        location: Joi.string().min(2).max(99).required(),
+
+    })
+
+    return joiSchema.validate(_reqBody);
+}
+
+exports.validLogin = (_reqBody) => {
+    let joiSchema = Joi.object({
+        email: Joi.string().min(2).max(99).email().required(),
+        password: Joi.string().min(3).max(99).required()
+    })
+
+    return joiSchema.validate(_reqBody);
+}
