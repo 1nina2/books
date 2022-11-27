7b7a852c5c48a0325e692db3f97cc9f2ac074f8f
prezman nina <prezmannina1989@gmail.com>
2022-11-27 18:36:11 +0200

    27/11/22

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,81 @@
 const express = require("express");
-const { userModel } = require("../models/userModel")
+const bcrypt = require("bcrypt");
+const { UserModel, userValid, loginValid } = require("../models/userModel")
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.json({ msg: "users work" })
+router.get("/", async(req, res) => {
+    let perPage = Math.min(req.query.perPage, 20) || 4;
+    let page = req.query.page || 1;
+    let sort = req.query.sort || "_id";
+    let reverse = req.query.reverse == "yes" ? -1 : 1;
+
+    try {
+        let data = await UserModel
+            .find({})
+            .limit(perPage)
+            .skip((page - 1) * perPage)
+            .sort({
+                [sort]: reverse
+            })
+        res.json(data);
+    } catch (err) {
+
+        console.log(err)
+        res.status(500).json({ msg: "err", err })
+    }
 })
+
 router.post("/", async(req, res) => {
-    // וולדזציה של הצד שרת שהמידע תקין TODO:
+    let valdiateBody = userValid(req.body);
+    if (valdiateBody.error) {
+        return res.status(400).json(valdiateBody.error.details)
+    }
+    try {
+        let user = new UserModel(req.body);
+        // הצפנה חד כיוונית לסיסמא ככה 
+        // שלא תשמר על המסד כמו שהיא ויהיה ניתן בקלות
+        // לגנוב אותה
+        user.password = await bcrypt.hash(user.password, 10)
+        await user.save();
+        // כדי להציג לצד לקוח סיסמא אנונימית
+        user.password = "******";
+        res.status(201).json(user)
+    } catch (err) {
+        // בודק אם השגיאה זה אימייל שקיים כבר במערכת
+        // דורש בקומפס להוסיף אינדקס יוניקי
+        if (err.code == 11000) {
+            return res.status(400).json({ msg: "Email already in system try login", code: 11000 })
+        }
+        console.log(err)
+        res.status(500).json({ msg: "err", err })
+    }
+})
 
-    // req.query , req.params
-    // נכניס מידע חדש למסד נתונים
-    // מידע שנשלח בפוסט יגיע במאפיין באדי בריק
+router.post("/login", async(req, res) => {
+    let valdiateBody = loginValid(req.body);
+    if (valdiateBody.error) {
+        return res.status(400).json(valdiateBody.error.details)
+    }
+    try {
+        // לבדוק אם המייל שנשלח בכלל יש רשומה של משתמש שלו
+        let user = await UserModel.findOne({ email: req.body.email })
+        if (!user) {
+            // שגיאת אבטחה שנשלחה מצד לקוח
+            return res.status(401).json({ msg: "User and password not match 1" })
+        }
+        // בדיקה הסימא אם מה שנמצא בבאדי מתאים לסיסמא המוצפנת במסד
+        let validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) {
+            return res.status(401).json({ msg: "User and password not match 2" })
+        }
+        // בשיעור הבא נדאג לשלוח טוקן למשתמש שיעזור לזהות אותו 
+        // לאחר מכן לראוטרים מסויימים
+        res.json({ msg: "Success, Need to send to client the token" });
+    } catch (err) {
 
-    let user = new UserModel(req.body);
-    await user.save();
-    // נקבל בדיוק את האובייקט שנשלח בבאדי לפי הסכימה
-    // כולל את האיי די החדש שלו
-    res.json(user);
+        console.log(err)
+        res.status(500).json({ msg: "err", err })
+    }
 })
 
 module.exports = router;
