5f54d7e543d7352e3a7ef8e07d050d12809ffc21
prezman nina <prezmannina1989@gmail.com>
2023-01-10 15:25:58 +0200

    10_01_2023

--- routes/config_routes.js
+++ routes/config_routes.js
@@ -1,9 +1,11 @@
 const indexR = require("./index");
 const usersR = require("./users");
 const booksR = require("./books");
+const catsR = require("./categories");
 
 exports.routesInit = (app) => {
     app.use("/", indexR);
     app.use("/users", usersR);
     app.use("/books", booksR);
+    app.use("/categories", catsR);
 }
